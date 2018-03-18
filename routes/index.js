ea5d87567da2762cb5966192f3b97d45aaf26bc1
Arne Fismen <arnefismen@gmail.com>
2018-03-18 23:16:28 +0100

    Small layout changes and some more routes.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const Router = require("koa-router");
 const router = new Router();
-const bots = require('../db/bots.js')
+const bots = require("../db/bots.js");
 
 router.get("/", async (ctx, next) => {
   await ctx.render("index");
+});
+
+router.get("/:botid", async (ctx, next) => {
+  await ctx.render("bot/");
+});
+
+router.get("/:botid/new", async (ctx, next) => {
+  await ctx.render("bot/new.pug");
 });
 
 module.exports = router;
