17969f6d4b449a4f6331f2269f16bdd6104cc9d6
Arne Fismen <arnefismen@gmail.com>
2018-03-19 20:48:51 +0100

    Prepared more templates

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
-const Router = require("koa-router");
+const Router = require('koa-router');
 const router = new Router();
-const bots = require("../db/bots.js");
+const bots = require('../db/bots.js');
 
-router.get("/", async (ctx, next) => {
-  await ctx.render("index");
+router.get('/', async (ctx, next) => {
+  await ctx.render('index');
 });
 
-router.get("/:botid", async (ctx, next) => {
-  await ctx.render("bot/");
+router.get('/b/:botid', async (ctx, next) => {
+  await ctx.render('bot/');
 });
 
-router.get("/:botid/new", async (ctx, next) => {
-  await ctx.render("bot/new.pug");
+router.get('/b/:botid/new', async (ctx, next) => {
+  await ctx.render('bot/new.pug');
+});
+
+router.get('/b/:botid/:eventid', async (ctx, next) => {
+  await ctx.render('bot/event.pug');
 });
 
 module.exports = router;
